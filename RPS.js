708fa450dcfa40865adbc4dc6615936e81f9441e
kodierer2021 <96021088+kodierer2021@users.noreply.github.com>
2022-09-01 20:43:45 +0200

    implement computer choice and game logic

--- RPS.js
+++ RPS.js
@@ -1,3 +1,43 @@
-console.log("Hello WORLD")
-let a = 5;
-console.log(a)
+/*a = Math.floor(Math.random() * 3);
+*/
+function getComputerChoice(){
+    let a, b;
+    a = Math.floor(Math.random() * 3);
+    if(a === 0){
+        b = 'Rock';
+    }
+    else if(a === 1){
+        b = 'Paper';
+    }
+    else{
+        b = 'Scissors';
+    }
+    console.log(b);    
+    return(b);
+}
+
+function determineOutcome(){
+    let outcome;
+    if (playerChoice === computerChoice){
+        outcome = 'draw';
+    }
+    else if(playerChoice === 'Rock' && computerChoice === 'Scissors'){
+        outcome ='win';
+    }
+    else if(playerChoice === 'Scissours' && computerChoice === 'Paper'){
+        outcome ='win';
+    }
+    else if(playerChoice === 'Paper' && computerChoice === 'Rock'){
+        outcome ='win';
+    }
+    else{
+        outcome = 'lose';
+    }
+    return(outcome);
+}
+
+let playerChoice, computerChoice, outcome;
+computerChoice = getComputerChoice();
+playerChoice = "Rock";
+outcome = determineOutcome();
+
